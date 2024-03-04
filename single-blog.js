3f1b9ccbaf7edf9ece9f95bb21040454f2d72343
M.RWEMA REMY <rwemaremy21@gmail.com>
2024-03-04 21:48:14 +0200

    now we can view blogs on admin view except delete and update

--- single-blog.js
+++ single-blog.js
@@ -1,28 +1,27 @@
 const currentUrl = new URL(window.location.href);
 const searchParams = new URLSearchParams(currentUrl.search);
 const blogId = searchParams.get("id");
+console.log(blogId);
 const allblog = "https://rwemaremy-my-brand-back-end.onrender.com";
-const selectblog = allblog.find((blog) => blog.id == blogId);
 
-const theblog = "";
-fetch(url + "/api/blogs/:id")
+const theblog = document.querySelector(".wrapper");
+fetch(allblog + `/api/blogs/${blogId}`)
   .then((res) => res.json())
   .then((give) => {
-    give.innerHTML = `
-<h2>${selectblog.title}</h2>
-<img src=${selectblog.image}>
+    theblog.innerHTML = `
+<h2>${give.title}</h2>
+<img src=${give.image}>
 <div class="textbox">
 
     <p>
-${selectblog.content}
+${give.content}
     </p>
     <div class="like">
     <div class="likes">
-         <img src="./assets/heart.png" onclick="liking()"><p id="single-display"></p>
+         <img src="./assets/heart.png" onclick="liking()"><p id="single-display">${give.likes}</p>
     </div>
-
-    <div class="likes">
-        <p> <img src="./assets/message.png">2k</p>
+    <div class="likes comment">
+        <p> <img src="./assets/message.png"></p>
     </div>
 
 </div>
