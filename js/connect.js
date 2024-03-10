358e379f279a497afa90753b1549f6f01195d61b
M.RWEMA REMY <rwemaremy21@gmail.com>
2024-03-11 00:08:02 +0200

    after removing restrictions from connect

--- js/connect.js
+++ js/connect.js
@@ -99,15 +99,15 @@ function logoutUser() {
   window.location.href = "/log-in.html";
 }
 
-function checkAuthentication() {
-  const token = localStorage.getItem("token");
-  if (!token) {
-    // Redirect to login page if token is not present
-    window.location.href = "/log-in.html";
-  }
-}
+// function checkAuthentication() {
+//   const token = localStorage.getItem("token");
+//   if (!token) {
+//     // Redirect to login page if token is not present
+//     window.location.href = "/log-in.html";
+//   }
+// }
 
-// Call checkAuthentication when the dashboard page loads
-window.addEventListener("DOMContentLoaded", () => {
-  checkAuthentication();
-});
+// // Call checkAuthentication when the dashboard page loads
+// window.addEventListener("DOMContentLoaded", () => {
+//   checkAuthentication();
+// });
